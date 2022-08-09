abe75fdbcf4f6f828381dd182ec94d02c99806b5
sadhan <sadhan@mystenlabs.com>
2022-08-09 01:47:23 -0700

    Cleanup mutex table periodically

--- crates/sui-storage/src/mutex_table.rs
+++ crates/sui-storage/src/mutex_table.rs
@@ -3,33 +3,80 @@
 
 use std::collections::hash_map::{DefaultHasher, RandomState};
 use std::collections::HashMap;
+use std::error::Error;
+use std::fmt;
 use std::hash::{BuildHasher, Hash, Hasher};
 use std::sync::Arc;
+use std::time::Duration;
 
 use tokio::sync::{Mutex, RwLock};
+use tokio::task::JoinHandle;
 
+type InnerLockTable<K> = HashMap<K, Arc<tokio::sync::Mutex<()>>>;
 // MutexTable supports mutual exclusion on keys such as TransactionDigest or ObjectDigest
 pub struct MutexTable<K: Hash> {
     random_state: RandomState,
-    lock_table: Vec<RwLock<HashMap<K, Arc<tokio::sync::Mutex<()>>>>>,
+    lock_table: Arc<Vec<RwLock<InnerLockTable<K>>>>,
     _k: std::marker::PhantomData<K>,
+    _cleaner: JoinHandle<()>,
 }
 
+#[derive(Debug)]
+pub enum TryAcquireLockError {
+    LockTableLocked,
+    LockEntryLocked,
+}
+
+impl fmt::Display for TryAcquireLockError {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(fmt, "operation would block")
+    }
+}
+
+impl Error for TryAcquireLockError {}
 // Opaque struct to hide tokio::sync::MutexGuard.
 pub struct LockGuard(tokio::sync::OwnedMutexGuard<()>);
 
-impl<'b, K: Hash + std::cmp::Eq + 'b> MutexTable<K> {
-    pub fn new(num_shards: usize, shard_size: usize) -> Self {
-        Self {
-            random_state: RandomState::new(),
-            lock_table: (0..num_shards)
+impl<K: Hash + std::cmp::Eq + Send + Sync + 'static> MutexTable<K> {
+    pub fn new_with_cleanup_duration(
+        num_shards: usize,
+        shard_size: usize,
+        duration: Duration,
+    ) -> Self {
+        let lock_table: Arc<Vec<RwLock<InnerLockTable<K>>>> = Arc::new(
+            (0..num_shards)
                 .into_iter()
                 .map(|_| RwLock::new(HashMap::with_capacity(shard_size)))
                 .collect(),
+        );
+        let cloned = lock_table.clone();
+        Self {
+            random_state: RandomState::new(),
+            lock_table,
             _k: std::marker::PhantomData {},
+            _cleaner: tokio::spawn(async move {
+                loop {
+                    Self::cleanup(cloned.clone());
+                    tokio::time::sleep(duration).await;
+                }
+            }),
         }
     }
-
+    pub fn new(num_shards: usize, shard_size: usize) -> Self {
+        Self::new_with_cleanup_duration(num_shards, shard_size, Duration::from_secs(60))
+    }
+    pub fn cleanup(lock_table: Arc<Vec<RwLock<InnerLockTable<K>>>>) {
+        for shard in lock_table.iter() {
+            let map = shard.try_write();
+            if map.is_err() {
+                continue;
+            }
+            map.unwrap().retain(|_k, v| {
+                let mutex_guard = v.try_lock();
+                mutex_guard.is_err()
+            });
+        }
+    }
     fn get_lock_idx(&self, key: &K) -> usize {
         let mut hasher = if !cfg!(test) {
             self.random_state.build_hasher()
@@ -72,4 +119,59 @@ impl<'b, K: Hash + std::cmp::Eq + 'b> MutexTable<K> {
             LockGuard(element.clone().lock_owned().await)
         }
     }
+
+    pub fn try_acquire_lock(&self, k: K) -> Result<LockGuard, TryAcquireLockError> {
+        let lock_idx = self.get_lock_idx(&k);
+        let res = self.lock_table[lock_idx].try_read();
+        if res.is_err() {
+            return Err(TryAcquireLockError::LockTableLocked);
+        }
+        let map = res.unwrap();
+        if let Some(element) = map.get(&k) {
+            let lock = element.clone().try_lock_owned();
+            if lock.is_err() {
+                return Err(TryAcquireLockError::LockEntryLocked);
+            }
+            Ok(LockGuard(lock.unwrap()))
+        } else {
+            // element doesn't exist
+            drop(map);
+            let res = self.lock_table[lock_idx].try_write();
+            if res.is_err() {
+                return Err(TryAcquireLockError::LockTableLocked);
+            }
+            let mut map = res.unwrap();
+            let element = map.entry(k).or_insert_with(|| Arc::new(Mutex::new(())));
+            let lock = element.clone().try_lock_owned();
+            Ok(LockGuard(lock.unwrap()))
+        }
+    }
+}
+
+#[tokio::test]
+async fn test_mutex_table() {
+    let mutex_table = MutexTable::<String>::new(1, 128);
+    let john1 = mutex_table.try_acquire_lock("john".to_string());
+    assert!(john1.is_ok());
+    let john2 = mutex_table.try_acquire_lock("john".to_string());
+    assert!(john2.is_err());
+    drop(john1);
+    let john2 = mutex_table.try_acquire_lock("john".to_string());
+    assert!(john2.is_ok());
+    let jane = mutex_table.try_acquire_lock("jane".to_string());
+    assert!(jane.is_ok());
+    MutexTable::cleanup(mutex_table.lock_table.clone());
+    let map = mutex_table.lock_table.get(0).as_ref().unwrap().try_read();
+    assert!(map.is_ok());
+    assert_eq!(map.unwrap().len(), 2);
+    drop(john2);
+    MutexTable::cleanup(mutex_table.lock_table.clone());
+    let map = mutex_table.lock_table.get(0).as_ref().unwrap().try_read();
+    assert!(map.is_ok());
+    assert_eq!(map.unwrap().len(), 1);
+    drop(jane);
+    MutexTable::cleanup(mutex_table.lock_table.clone());
+    let map = mutex_table.lock_table.get(0).as_ref().unwrap().try_read();
+    assert!(map.is_ok());
+    assert!(map.unwrap().is_empty());
 }
